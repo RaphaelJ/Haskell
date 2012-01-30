d8acb20405eae691108799489cde395c746ed0c6
Raphael Javaux <raphaeljavaux@gmail.com>
2012-01-30 19:53:15 +0100

    refactoring utils.hs

--- project_euler/1.hs
+++ project_euler/1.hs
@@ -1,5 +1,5 @@
-import Utils (divises)
+import Utils (divides)
 
 main = print $ sum $ [ x | x <- [1..999], mult3or5 x ]
   where
-    mult3or5 x = 5 `divises`s x || 3 `divises` x
+    mult3or5 x = 5 `divides`s x || 3 `divides` x
